c3ced1108bf7c80b7f8aeb1bfbfbbdc7daac58ae
Delio Marcos <deliomarcos.contato@gmail.com>
2022-02-06 23:48:36 -0300

    Corrigindo problema de conversao de String[] para String;

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -8,7 +8,15 @@ class ShowUserProfileController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.headers;
 
-    return response.json(this.showUserProfileUseCase.execute({ user_id }));
+    try {
+      const user = this.showUserProfileUseCase.execute({
+        user_id,
+      });
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
